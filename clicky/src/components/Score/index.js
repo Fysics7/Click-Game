fef5a6bb669ada1ff8973227475501959fcee9e7
fysics <j.mills.ucf.cbc@gmail.com>
2018-09-22 01:04:19 -0400

    Functioning as Intended

--- clicky/src/components/Score/index.js
+++ clicky/src/components/Score/index.js
@@ -1,1 +1,1 @@
-export { default } from "./Scores";
+export { default } from "./Score";
--- clicky/src/components/Score/score.js
+++ clicky/src/components/Score/score.js
@@ -1,14 +1,13 @@
 import React from "react";
-import "./score.css";
-import Arrow from 'react-icons/lib/fa/caret-right';
+import "./Score.css";
 
 //stateless component
 const Score = props => (
     <div className="userScore">
-        <h3 className="score">Your Current Score <Arrow /> {props.total}</h3>
+        <h3 className="score">Your Current Score {props.total}</h3>
         <h3 className="status">{props.status}</h3>
     </div>
 
 );
 
-export default Score
+export default Score;
